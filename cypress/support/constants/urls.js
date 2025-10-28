c2f04c2d47bd61dbc90fecc391ae77c6c0dc8222
Gawali <2409100@cognizant.com>
2025-10-28 18:04:19 +0530

    Updated the URLs in seperate file

---
+++ cypress/support/constants/urls.js
@@ -0,0 +1,11 @@
+const BASE_URL = '/#/';
+const OKTA_URL = 'https://virginmediao2.oktapreview.com';
+const OKTA_SSO_PATH = '/app/amazon_aws/exk5do4cjfiOd1ppu0x7/sso/saml';
+const Omni_Home_URL = '/#/home/agent';
+
+module.exports = {
+  BASE_URL,
+  OKTA_URL,
+  OKTA_SSO_PATH,
+  Omni_Home_URL
+};
--- cypress/support/pages/LoginPage.js
+++ cypress/support/pages/LoginPage.js
@@ -1,28 +1,34 @@
 const { overrideWindowOpen } = require('../utils/GenericUtils');
 const { waitFor, getWithTimeout, getTextAndAssert } = require('../utils/waitUtils');
+const { BASE_URL, OKTA_URL, OKTA_SSO_PATH } = require('../constants/urls');
 
 class LoginPage {
   visit() {
-    cy.visit('/#/');
+    cy.visit(BASE_URL);
   }
 
   clickLoginButton() {
     getWithTimeout('#omni_dashboard_login_btn').click()
   }
 
-  loginOkta(username, password) {
-    cy.origin('https://virginmediao2.oktapreview.com', { args: { username, password } }, ({ username, password }) => {
-      cy.visit('/app/amazon_aws/exk5do4cjfiOd1ppu0x7/sso/saml');
+  
+loginOkta(username, password) {
+  cy.origin(
+    OKTA_URL,
+    { args: { username, password, oktaPath: OKTA_SSO_PATH } },
+    ({ username, password, oktaPath }) => {
+      cy.visit(oktaPath);
       cy.get('#identifier', { timeout: 10000 }).type(username);
       cy.get('button[data-se="save"]').click();
       cy.get('input[data-se="credentials.passcode"]').type(password);
       cy.get('button[data-se="save"]').click();
       cy.get('#oktaVerifyPush_1-description').click();
-    });
-  }
+    }
+  );
+}
 
   verifyLoginPage() {
-    cy.url().should('include', '/#/');
+    cy.url().should('include', BASE_URL);
     getWithTimeout('#omni_dashboard_login_btn').should('be.visible');
   }
 }
